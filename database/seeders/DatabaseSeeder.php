853efe5ed7e442829923c0ca8af8315d8dc3339e
zaher-sankari <zaher.lp1@gmail.com>
2025-12-29 19:04:42 +0300

    remove Room seeders from data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,7 +19,6 @@ class DatabaseSeeder extends Seeder
         $this->call([
             RoleSeeder::class,
             DevUsersSeeder::class,
-            RoomSeeder::class
         ]);
 
         User::factory()->create([
