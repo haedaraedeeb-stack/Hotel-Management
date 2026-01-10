bb7eea75a5c05a47fc67df01da1b311d36ba77c7
Haedara Deeb <haedaraedeeb@gmail.com>
2026-01-10 17:52:11 +0300

    Update bootstrap

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -7,9 +7,6 @@ use Spatie\Permission\Middleware\PermissionMiddleware;
 use Spatie\Permission\Middleware\RoleMiddleware;
 use Spatie\Permission\Middleware\RoleOrPermissionMiddleware;
 
-use Spatie\Permission\Middleware\PermissionMiddleware;
-use Spatie\Permission\Middleware\RoleMiddleware;
-use Spatie\Permission\Middleware\RoleOrPermissionMiddleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
